c96d7aaafdf88e36ece58e72aced5197927dc36c
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-10-11 21:05:01 -0300

    feat(lang): create base pt auth dictionary

---
+++ resources/lang/pt/auth.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Essas credenciais não correspondem aos nossos registros.',
+    'password' => 'A senha fornecida está incorreta.',
+    'throttle' => 'Muitas tentativas de login. Tente novamente em :seconds segundos.',
+    'user_registered' => 'Usuário registrado!',
+    'user_logged_in' => 'Usuário logado com sucesso',
+    'user_logged_out' => 'Usuário deslogado',
+    'user_already_logged_out' => 'Usuário já deslogado',
+    'user_blocked' => 'Usuário bloqueado',
+    'user_pendent' => 'Usuário pendente',
+    'user_pendent_or_blocked' => 'Seu usuário está pendente ou bloqueado',
+    'user_not_found' => 'Usuário não encontrado',
+    'unauthenticated' => 'Não autenticado',
+    'access_denied' => 'Acesso negada',
+    'token_expired' => 'Token expirado',
+    'token_invalid' => 'Token inválido',
+    'token_blacklisted' => 'Token na lista negra',
+    'token_not_provided' => 'Token não fornecido',
+    'token_cannot_parse' => 'Token não pode ser formatado',
+];

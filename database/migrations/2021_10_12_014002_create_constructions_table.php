d730984634f6a7f81c842810c44c9a73b8eaf19d
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-10-12 11:32:05 -0300

    Feat/vic generate migration (#21)
    * feat(migration): create users table
    
    * feat(migration): create user password resets table
    
    * feat(migration): create failed jobs table
    
    * feat(migration): create user personal access tokens table
    
    * feat(migration): create user two factor columns table
    
    * feat(migration): create notifications table
    
    * feat(migration): create constructions table
    
    * feat(migration): create constructions users table
    
    * feat(migration): create constructions stages table
    
    * feat(migration): create constructions inspections table
    
    * feat(migration): create providers table
    
    * feat(migration): create products table
    
    * feat(migration): create stocks table
    
    * 0.0.0.6: app version

---
+++ database/migrations/2021_10_12_014002_create_constructions_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use App\Enums\ConstructionStatus;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateConstructionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('constructions', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->timestamp('start_date');
+            $table->timestamp('end_date');
+            $table->enum('status', ConstructionStatus::toArray())->default(ConstructionStatus::PENDENT);
+            $table->decimal('budget', 10)->default(0.00);
+            $table->string('project')->nullable();
+            $table->timestamp('canceled_at')->nullable();
+            $table->timestamp('started_at')->nullable();
+            $table->timestamp('finalized_at')->nullable();
+            $table->timestamp('abandoned_at')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('constructions');
+    }
+}
---
+++ database/migrations/2021_10_12_014337_create_stocks_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use App\Enums\StockFlow;
+use App\Enums\StockStatus;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStocksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stocks', function (Blueprint $table) {
+            $table->id();
+            $table->decimal('quantity', 10)->default(0.00);
+            $table->decimal('price', 10)->default(0.00);
+            $table->foreignId('construction_id')->references('id')->on('constructions');
+            $table->foreignId('provider_id')->references('id')->on('providers');
+            $table->foreignId('product_id')->references('id')->on('products');
+            $table->enum('flow', StockFlow::toArray());
+            $table->enum('status', StockStatus::toArray())->default(StockStatus::PENDENT);
+            $table->string('outgoing_receiver')->nullable();
+            $table->string('receipt')->nullable();
+            $table->timestamp('canceled_at')->nullable();
+            $table->timestamp('arrived_at')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stocks');
+    }
+}

87a6740f277d2be2a6b84756e44a0a84587eb6f2
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-10-12 12:47:16 -0300

    feat(model): generate provider model

---
+++ app/Models/Provider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Provider extends Model
+{
+    use SoftDeletes, HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['name'];
+}

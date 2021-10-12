bfab6b52698a4b39b0809e051e4253ecc4100fdd
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-10-12 11:29:02 -0300

    feat(migration): create constructions inspections table

---
+++ database/migrations/2021_10_12_014307_create_construction_inspections_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateConstructionInspectionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('construction_inspections', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('construction_id')->references('id')->on('constructions');
+            $table->timestamp('seem');
+            $table->string('report')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('construction_inspections');
+    }
+}

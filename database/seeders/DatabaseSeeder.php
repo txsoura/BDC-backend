d2cf2c6d30e4c5d1025e7beccda25d64b884f2c7
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-10-12 13:11:38 -0300

    feat(seed): register db seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call([
+            UserSeeder::class,
+            ConstructionSeeder::class,
+            ConstructionUserSeeder::class,
+            ConstructionStageSeeder::class,
+            ConstructionInspectionSeeder::class,
+            ProviderSeeder::class,
+            ProductSeeder::class,
+            StockSeeder::class,
+        ]);
     }
 }

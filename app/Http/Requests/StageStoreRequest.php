99093ff50bbe6fdf9aa791a429debe162759cf35
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-10-16 23:31:02 -0300

    feat(request): create stage store validator

---
+++ app/Http/Requests/StageStoreRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Txsoura\Core\Http\Requests\CoreRequest;
+
+class StageStoreRequest extends CoreRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'construction_id' => 'required|numeric|exists:constructions,id',
+            'budget' => 'required|numeric|min:0',
+        ];
+    }
+
+    /**
+     * Prepare the data for validation.
+     *
+     * @return void
+     */
+    protected function prepareForValidation()
+    {
+        return $this->merge([
+            'name' => ucwords($this->name),
+        ]);
+    }
+}

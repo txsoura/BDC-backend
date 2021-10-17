85476f03a42f2b97c7db4010f90d22f892fbedfc
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-10-16 22:28:26 -0300

    feat(model): generate provider helpers & add relations

--- app/Models/Provider.php
+++ app/Models/Provider.php
@@ -2,10 +2,42 @@
 
 namespace App\Models;
 
+use Database\Factories\ProviderFactory;
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Carbon;
 
+/**
+ * App\Models\Provider
+ *
+ * @property int $id
+ * @property string $name
+ * @property int $construction_id
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property Carbon|null $deleted_at
+ * @property-read Construction $construction
+ * @property-read Collection|Stock[] $stocks
+ * @property-read int|null $stocks_count
+ * @method static ProviderFactory factory(...$parameters)
+ * @method static Builder|Provider newModelQuery()
+ * @method static Builder|Provider newQuery()
+ * @method static \Illuminate\Database\Query\Builder|Provider onlyTrashed()
+ * @method static Builder|Provider query()
+ * @method static Builder|Provider whereConstructionId($value)
+ * @method static Builder|Provider whereCreatedAt($value)
+ * @method static Builder|Provider whereDeletedAt($value)
+ * @method static Builder|Provider whereId($value)
+ * @method static Builder|Provider whereName($value)
+ * @method static Builder|Provider whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|Provider withTrashed()
+ * @method static \Illuminate\Database\Query\Builder|Provider withoutTrashed()
+ * @mixin Eloquent
+ */
 class Provider extends Model
 {
     use SoftDeletes, HasFactory;
@@ -15,5 +47,15 @@ class Provider extends Model
      *
      * @var array
      */
-    protected $fillable = ['name'];
+    protected $fillable = ['name', 'construction_id'];
+
+    public function construction()
+    {
+        return $this->belongsTo(Construction::class);
+    }
+
+    public function stocks()
+    {
+        return $this->hasMany(Stock::class);
+    }
 }

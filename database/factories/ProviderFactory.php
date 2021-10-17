f9531719b8e694de5fe6cc456cee816983a1aac5
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-10-17 00:21:50 -0300

    refactor(factory): update provider

--- database/factories/ProviderFactory.php
+++ database/factories/ProviderFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Construction;
 use App\Models\Provider;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
@@ -23,6 +24,7 @@ class ProviderFactory extends Factory
     {
         return [
             'name' => $this->faker->name(),
+            'construction_id' => Construction::factory(),
         ];
     }
 }

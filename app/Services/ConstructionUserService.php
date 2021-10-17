e59b77e5b88355479de07304ba715f39684e4c91
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-10-17 00:08:01 -0300

    Feat/vic generate services (#37)
    * feat(support): create base construction CRUD methods service
    
    * feat(service): create construction
    
    * feat(service): create construction user
    
    * feat(service): create stage
    
    * feat(service): create user
    
    * feat(service): create stock
    
    * feat(service): create provider
    
    * feat(service): create product
    
    * feat(service): create inspection
    
    * 0.0.1.6: app version

---
+++ app/Services/ConstructionUserService.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Services;
+
+use App\Http\Requests\ConstructionUserStoreRequest;
+use App\Http\Requests\ConstructionUserUpdateRequest;
+use App\Models\ConstructionUser;
+use App\Repositories\ConstructionUserRepository;
+use App\Support\Traits\BaseConstructionCRUDMethodsService;
+use Txsoura\Core\Services\CoreService;
+
+class ConstructionUserService extends CoreService
+{
+    use BaseConstructionCRUDMethodsService;
+
+    protected $storeRequest = ConstructionUserStoreRequest::class;
+    protected $updateRequest = ConstructionUserUpdateRequest::class;
+
+    /**
+     * @var ConstructionUserRepository
+     */
+    protected $repository;
+
+    /**
+     * ConstructionUserService constructor.
+     * @param ConstructionUserRepository $repository
+     */
+    public function __construct(ConstructionUserRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    /**
+     * Model class for crud.
+     *
+     * @return string
+     */
+    protected function model(): string
+    {
+        return ConstructionUser::class;
+    }
+}

e02c615bc382313b99053503947253aed4586e1c
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-10-16 23:44:09 -0300

    feat(resource): create user

---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param Request $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'status' => $this->status,
+            'fcm_token' => $this->fcm_token,
+            'role' => $this->role,
+            'email_verified_at' => $this->email_verified_at,
+            'constructions' => ConstructionUserResource::collection($this->whenLoaded('constructions')),
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+            'deleted_at' => $this->deleted_at
+        ];
+    }
+}

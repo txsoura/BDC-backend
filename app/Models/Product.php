7feec7443692833a107705ca32856802c4570596
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-10-16 22:28:42 -0300

    feat(model): generate product helpers & add relations

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,10 +2,48 @@
 
 namespace App\Models;
 
+use Database\Factories\ProductFactory;
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Carbon;
 
+/**
+ * App\Models\Product
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $type
+ * @property int $notify_when_stock_below
+ * @property mixed $available
+ * @property int $construction_id
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property Carbon|null $deleted_at
+ * @property-read Construction $construction
+ * @property-read Collection|Stock[] $stocks
+ * @property-read int|null $stocks_count
+ * @method static ProductFactory factory(...$parameters)
+ * @method static Builder|Product newModelQuery()
+ * @method static Builder|Product newQuery()
+ * @method static \Illuminate\Database\Query\Builder|Product onlyTrashed()
+ * @method static Builder|Product query()
+ * @method static Builder|Product whereAvailable($value)
+ * @method static Builder|Product whereConstructionId($value)
+ * @method static Builder|Product whereCreatedAt($value)
+ * @method static Builder|Product whereDeletedAt($value)
+ * @method static Builder|Product whereId($value)
+ * @method static Builder|Product whereName($value)
+ * @method static Builder|Product whereNotifyWhenStockBelow($value)
+ * @method static Builder|Product whereType($value)
+ * @method static Builder|Product whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|Product withTrashed()
+ * @method static \Illuminate\Database\Query\Builder|Product withoutTrashed()
+ * @mixin Eloquent
+ */
 class Product extends Model
 {
     use SoftDeletes, HasFactory;
@@ -15,7 +53,7 @@ class Product extends Model
      *
      * @var array
      */
-    protected $fillable = ['name', 'type', 'notify_when_stock_below', 'available',];
+    protected $fillable = ['name', 'type', 'notify_when_stock_below', 'available', 'construction_id'];
 
     /**
      * The attributes that should be cast.
@@ -36,4 +74,14 @@ class Product extends Model
         'notify_when_stock_below' => '0',
         'available' => '0.00'
     ];
+
+    public function construction()
+    {
+        return $this->belongsTo(Construction::class);
+    }
+
+    public function stocks()
+    {
+        return $this->hasMany(Stock::class);
+    }
 }

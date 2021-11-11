6b486664e29429e14461c199d357692d077370ba
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-11-11 01:25:42 -0300

    feat(lang): create company es dictionary

---
+++ resources/lang/en/company.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'approve_failed' => 'Company approves failed',
+    'approved' => 'Company approved',
+    'block_failed' => 'Company block failed',
+    'blocked' => 'Company blocked',
+];

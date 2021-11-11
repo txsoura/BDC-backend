bcaf551c4648401ed0aa4175373ce7b7501d4947
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-11-10 22:39:47 -0300

    feat(lang): create user en dictionary

---
+++ resources/lang/en/user.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'approve_failed' => 'User approves failed',
+    'approved' => 'User approved',
+    'block_failed' => 'User block failed',
+    'blocked' => 'User blocked',
+];

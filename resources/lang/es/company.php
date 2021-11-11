b8115eb7669e0ed406e7290f8b0bae7131e8dbe0
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-11-11 01:25:54 -0300

    feat(lang): create company en dictionary

---
+++ resources/lang/es/company.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'approve_failed' => 'Aprobación de empresa fallida',
+    'approved' => 'Empresa aprobada',
+    'block_failed' => 'Falló el bloqueo de empresa',
+    'blocked' => 'Empresa bloqueada',
+];

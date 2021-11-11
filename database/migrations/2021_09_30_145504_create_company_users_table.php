2a88346ea049088e2dddf921a54e659b453660a9
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-11-11 00:32:35 -0300

    feat(migration): create company users table

---
+++ database/migrations/2021_09_30_145504_create_company_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Enums\CompanyUserRole;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCompanyUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('company_users', function (Blueprint $table) {
+            $table->id();
+            $table->enum('role', CompanyUserRole::toArray())->default(CompanyUserRole::MEMBER);
+            $table->foreignId('user_id')->references('id')->on('users');
+            $table->foreignId('company_id')->references('id')->on('companies');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('company_users');
+    }
+}

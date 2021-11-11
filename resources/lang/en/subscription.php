571110e23ff121595df0d3806af4ace6af2a491f
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-11-11 01:28:08 -0300

    feat(lang): create subscription en dictionary

---
+++ resources/lang/en/subscription.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'activate_failed' => 'Subscription activation failed',
+    'activated' => 'Subscription activated',
+    'deactivate_failed' => 'Subscription deactivate failed',
+    'deactivated' => 'Subscription deactivated',
+    'activate' => [
+        'message' => 'Cannot activate the subscription',
+        'error' => 'Only valid subscriptions can be approved'
+    ],
+    'deactivate' => [
+        'message' => 'Cannot deactivate the subscription',
+        'error' => 'Only pendent subscriptions can be deactivated'
+    ],
+];

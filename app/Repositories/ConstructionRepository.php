e321d02c1f4b2820d1f71de1b2ea56bace8519ac
Victor Tesoura Júnior <txsoura@yahoo.com>
2022-01-28 20:54:51 -0300

    Get construction by user and company (#83)
    * feat(repository): improve all to only get constructions by user and company
    
    * feat(repository): improve find to only get construction by user and company
    
    * feat(repository): improve findOrFail to only get construction by user and company
    
    * feat(repository): improve findOrFailWithTrashed to only get construction by user and company
    
    * 0.0.4.1: app version

--- app/Repositories/ConstructionRepository.php
+++ app/Repositories/ConstructionRepository.php
@@ -4,13 +4,15 @@ namespace App\Repositories;
 
 use App\Enums\ConstructionStatus;
 use App\Models\Construction;
-use App\Support\Traits\GetAllWithFilters;
+use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Str;
 use Txsoura\Core\Helpers;
 use Txsoura\Core\Repositories\Traits\QueryFilterRepository;
 
 class ConstructionRepository
 {
-    use Helpers, QueryFilterRepository, GetAllWithFilters;
+    use Helpers, QueryFilterRepository;
 
     /**
      * Allow model relations to use in include
@@ -49,6 +51,98 @@ class ConstructionRepository
     protected $allow_between_values = array('budget');
 
     /**
+     * Get all model data, with filters if required
+     *
+     * @return Collection|Paginator
+     */
+    public function all()
+    {
+        $this->setParams($this->checkWhereColumns());
+
+        $models = Construction::when(key_exists('onlyTrashed', $this->request), function ($query) {
+            if ($this->request['onlyTrashed'])
+                $query->onlyTrashed();
+        })
+            ->when(key_exists('withTrashed', $this->request), function ($query) {
+                if ($this->request['withTrashed'])
+                    $query->withTrashed();
+
+                return $query;
+            })
+            ->whereCompanyId($this->request['company_id'])
+            ->whereHas('users.companyUser', function ($query) {
+                $query->where('user_id', auth()->user()->id);
+            })
+            ->when(key_exists('date_column', $this->request), function ($query) {
+                if ($this->checkDateColumns()) {
+                    if (key_exists('date_start', $this->request))
+                        $query->where($this->request['date_column'], '>=', $this->request['date_start']);
+                    if (key_exists('date_end', $this->request))
+                        $query->where($this->request['date_column'], '<=', $this->request['date_end']);
+                }
+
+                return $query;
+            })
+            ->when(key_exists('value_column', $this->request), function ($query) {
+                if ($this->checkValueColumns()) {
+                    if (key_exists('value_min', $this->request))
+                        $query->where($this->request['value_column'], '>=', $this->request['value_min']);
+                    if (key_exists('value_max', $this->request))
+                        $query->where($this->request['value_column'], '<=', $this->request['value_max']);
+                }
+
+                return $query;
+            })
+            ->when(key_exists('q', $this->request), function ($query) {
+                if ($this->checkQueryColumns()) {
+                    foreach ($this->allow_like as $column) {
+                        $query->orWhere($column, 'like', '%' . $this->request['q'] . '%');
+                    }
+                }
+
+                return $query;
+            })
+            ->when($this->params, function ($query) {
+                foreach (array_keys($this->params) as $column) {
+                    $query->where($column, $this->params[$column]);
+                }
+
+                return $query;
+            })
+            ->when(key_exists('sort', $this->request), function ($query) {
+                $columns = explode(',', $this->request['sort']);
+
+                if ($this->checkSortColumns($columns)) {
+                    foreach ($columns as $column) {
+                        if (Str::contains($column, '-')) {
+                            $query->orderBy(substr($column, 1), 'desc');
+                        } else {
+                            $query->orderBy($column, 'asc');
+                        }
+                    }
+                }
+
+                return $query;
+            })
+            ->when(key_exists('take', $this->request), function ($query) {
+                return $query->limit($this->request['take']);
+            })
+            ->when(key_exists('include', $this->request), function ($query) {
+                if ($this->checkIncludeColumns()) {
+                    $query->with(explode(',', $this->request['include']));
+                }
+
+                return $query;
+            });
+
+        if (key_exists('paginate', $this->request)) {
+            return $models->paginate($this->request['paginate']);
+        } else {
+            return $models->get();
+        }
+    }
+
+    /**
      * @param int $id
      * @param int $companyId
      * @return Construction|null
@@ -57,6 +151,9 @@ class ConstructionRepository
     {
         return Construction::whereCompanyId($companyId)
             ->whereId($id)
+            ->whereHas('users.companyUser', function ($query) {
+                $query->where('user_id', auth()->user()->id);
+            })
             ->when($this->request, function ($query) {
                 if (key_exists('include', $this->request))
                     $query->with(explode(',', $this->request['include']));
@@ -75,6 +172,9 @@ class ConstructionRepository
     {
         return Construction::whereCompanyId($companyId)
             ->whereId($id)
+            ->whereHas('users.companyUser', function ($query) {
+                $query->where('user_id', auth()->user()->id);
+            })
             ->when($this->request, function ($query) {
                 if (key_exists('include', $this->request))
                     $query->with(explode(',', $this->request['include']));
@@ -94,6 +194,9 @@ class ConstructionRepository
         return Construction::withTrashed()
             ->whereCompanyId($companyId)
             ->whereId($id)
+            ->whereHas('users.companyUser', function ($query) {
+                $query->where('user_id', auth()->user()->id);
+            })
             ->when(key_exists('include', $this->request), function ($query) {
                 if ($this->checkIncludeColumns()) {
                     $query->with(explode(',', $this->request['include']));
